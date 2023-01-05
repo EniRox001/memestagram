4f984b892d78157d3cfaf327ace725cccb21323c
EniRox001 <enirox001@gmail.com>
2023-01-05 09:07:35 +0100

    completed phone number signup field

--- lib/widgets/mobile/signup_mobile_two.dart
+++ lib/widgets/mobile/signup_mobile_two.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl_phone_field/country_picker_dialog.dart';
 import 'package:intl_phone_field/intl_phone_field.dart';
 import 'package:memestagram/utils/colors.dart';
 import 'package:memestagram/widgets/common/w_elevated_button.dart';
@@ -50,7 +51,51 @@ class _SignupMobileTwoState extends State<SignupMobileTwo>
             padding: EdgeInsets.all(20.0.sp),
             child: Column(
               children: <Widget>[
-                IntlPhoneField(),
+                IntlPhoneField(
+                  initialCountryCode: 'NG',
+                  flagsButtonMargin: const EdgeInsets.only(left: 20.0),
+                  flagsButtonPadding: const EdgeInsets.only(right: 10.0),
+                  showCountryFlag: true,
+                  showDropdownIcon: false,
+                  pickerDialogStyle: PickerDialogStyle(
+                    searchFieldPadding: EdgeInsets.zero,
+                    searchFieldInputDecoration: InputDecoration(
+                      hintText: 'Search Country',
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          color: Colors.black87,
+                        ),
+                        borderRadius: BorderRadius.circular(
+                          10.0,
+                        ),
+                      ),
+                      border: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Colors.black12),
+                        borderRadius: BorderRadius.circular(
+                          10.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                  decoration: InputDecoration(
+                    hintText: 'Phone Number',
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        color: Colors.black87,
+                      ),
+                      borderRadius: BorderRadius.circular(
+                        10.0,
+                      ),
+                    ),
+                    border: OutlineInputBorder(
+                      borderSide: const BorderSide(color: Colors.black12),
+                      borderRadius: BorderRadius.circular(
+                        10.0,
+                      ),
+                    ),
+                  ),
+                  onChanged: (phone) {},
+                ),
                 WElevatedButton(onPressed: () {}, text: 'Next')
               ],
             ),
